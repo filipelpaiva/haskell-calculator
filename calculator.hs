8e07c9967f63ce33d55de25e5ba7375ec6589c45
filipelpaiva <fpaiva@junifeup.pt>
2025-11-17 01:47:50 +0000

    Part 2

--- calculator.hs
+++ calculator.hs
@@ -14,23 +14,42 @@ import Data.Char
 -- a data type for expressions
 -- made up from integer numbers, + and *
 --
+type Name = String
+type Env = [(Name, Integer)]
+
 data Expr = Num Integer
           | Add Expr Expr
           | Mul Expr Expr
           | Sub Expr Expr
           | Div Expr Expr
           | Mod Expr Expr
+          | Var Name
           deriving Show
+
+data Command = Assign Name Expr
+              | Eval Expr
+              deriving Show
+
+-- searches for a variable in an environment
+findVarEnv :: Name -> Env -> Integer
+findVarEnv n env = case lookup n env of
+              Just val -> val
+              Nothing -> error ("Variavel não definida: " ++ n)
+
+-- add a variable (name and integer) to an environment and updates it
+update :: Name -> Integer -> Env -> Env
+update name val env = (name, val) : filter (\(n,_) -> n /= name) env
 
 -- a recursive evaluator for expressions
 --
-eval :: Expr -> Integer
-eval (Num n) = n
-eval (Add e1 e2) = eval e1 + eval e2
-eval (Mul e1 e2) = eval e1 * eval e2
-eval (Sub e1 e2) = eval e1 - eval e2
-eval (Div e1 e2) = eval e1 `div` eval e2
-eval (Mod e1 e2) = eval e1 `mod` eval e2
+eval :: Env -> Expr -> Integer
+eval env (Num n) = n
+eval env (Add e1 e2) = eval env e1 + eval env e2
+eval env (Mul e1 e2) = eval env e1 * eval env e2
+eval env (Sub e1 e2) = eval env e1 - eval env e2
+eval env (Div e1 e2) = eval env e1 `div` eval env e2
+eval env (Mod e1 e2) = eval env e1 `mod` eval env e2
+eval env (Var n) = findVarEnv n env
 
 -- | a parser for expressions
 -- Grammar rules:
@@ -41,6 +60,19 @@ eval (Mod e1 e2) = eval e1 `mod` eval e2
 -- termCont ::= '*' factor termCont | epsilon
 
 -- factor ::= natural | '(' expr ')'
+
+variable :: Parser Name
+variable = many1 (satisfy isLetter)
+
+command :: Parser Command
+command =  do v <- variable
+              char '='
+              e <- expr
+              return (Assign v e)
+          <|>
+           do e <- expr
+              return (Eval e)
+
 
 expr :: Parser Expr
 expr = do t <- term
@@ -77,14 +109,20 @@ termCont acc =
                     termCont (Mod acc f))
                 <|> return acc
 
+
 factor :: Parser Expr
-factor = do n <- natural
-            return (Num n)
-          <|>
-          do char '('
-             e <- expr
-             char ')'
-             return e
+factor =
+        (do v <- variable
+            (return (Var v)))
+        <|>
+          (do n <- natural
+              return (Num n))
+        <|>
+        (do char '('
+            e <- expr
+            char ')'
+            return e)
+
              
 
 natural :: Parser Integer
@@ -92,21 +130,47 @@ natural = do xs <- many1 (satisfy isDigit)
              return (read xs)
 
 ----------------------------------------------------------------             
-  
+
 main :: IO ()
 main
   = do txt <- getContents
-       calculator (lines txt)
+       -- Start the 'calculator' loop with an EMPTY environment ([])
+       calculator [] (lines txt)
 
--- | read-eval-print loop
-calculator :: [String] -> IO ()
-calculator []  = return ()
-calculator (l:ls) = do putStrLn (evaluate l)
-                       calculator ls  
+-- | read-eval-print loop (now stateful)
+--   Receives the CURRENT environment and the remaining lines
+calculator :: Env -> [String] -> IO ()
+calculator env []  = return () -- No more lines, terminate
+calculator env (l:ls) = do
+    -- Ignore empty lines (to avoid errors)
+    if null l
+    then calculator env ls
+    else do
+      -- Execute line 'l' with the current environment 'env'
+      let (output, newEnv) = execute env l
+      -- Print the result
+      putStrLn output
+      -- Recurse with the NEW environment (newEnv)
+      -- and the rest of the lines (ls)
+      calculator newEnv ls
 
--- | evaluate a single expression
-evaluate :: String -> String
-evaluate txt
-  = case parse expr txt of
-      [ (tree, "") ] ->  show (eval tree)
-      _ -> "parse error; try again"  
+-- | execute a single command
+--   This is the main function that replaces 'evaluate'
+execute :: Env -> String -> (String, Env)
+execute env txt =
+  case parse command txt of -- <-- Parse a 'command'
+
+    -- Case 1: The command was 'Eval expr'
+    -- The parser succeeded and left no remaining text ("")
+    [(Eval expr, "")] -> (show value, env) -- Return the value, and the UNCHANGED env
+      where
+        value = eval env expr  -- Evaluate the expression in the current env
+
+    -- Case 2: The command was 'Assign name expr'
+    [(Assign name expr, "")] -> (show value, newEnv) -- Return the value, and the NEW env
+      where
+        value  = eval env expr           -- Evaluate the expression in the current env
+        newEnv = update name value env -- Create a NEW env with the variable
+
+    -- Case 3: The parse failed (e.g., '[]') or left text (e.g., '[(... ,"abc")]')
+    _ -> ("parse error", env) -- Return error, and the UNCHANGED env
